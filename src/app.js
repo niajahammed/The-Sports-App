31d8a57742c013d05dcdfae9bf89942638b95138
Niaj Ahammed Shuvo <shuvoniaj@gmail.com>
2026-08-19 20:22:48 +0600

    d

--- src/app.js
+++ src/app.js
@@ -1,21 +1,3 @@
-async function fetchTeam () {
-  let URL = `https://www.thesportsdb.com/api/v1/json/123/lookuptable.php?l=4328`;
+const API_KEY = "123";
+const BASE_URL = `https://www.thesportsdb.com/api/v1/json/${API_KEY}`;
 
-  await fetch(URL)
-        .then((res) => res.json())
-        .then((data) => console.log(data.table));
-}
-
-function showTeam (teams) {
-  teams.forEach((team) => {
-    document.getElementById("display").innerHTML = `
-      <div>
-        <img src="${team.strBadge}" alt="">
-        <img src="${team.strBanner}" alt="">
-        <h2>${team.strCountry}</h2>
-        <p>${team.strDescriptionEN}</p>
-      </div>
-    `;
-  })
-}
-fetchTeam();
